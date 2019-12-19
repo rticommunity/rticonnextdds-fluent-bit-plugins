ec1e863c45565f89aae4349cecb346ba0ae6170b
Fabrizio Bertocci <fabrizio@rti.com>
2019-12-19 09:02:14 -0500

    Removed the header file

--- src/out_dds_unstr/out_dds_unstr.c
+++ src/out_dds_unstr/out_dds_unstr.c
@@ -9,7 +9,29 @@
  use the software.
  ******************************************************************************/
 
-#include "out_dds_unstr.h"
+#include <fluent-bit/flb_info.h>
+#include <fluent-bit/flb_output.h>
+#include <fluent-bit/flb_time.h>
+#include <fluent-bit/flb_pack.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "fb.h"
+#include "fbSupport.h"
+#include "fbcommon.h"
+
+struct flb_out_dds_unstr_config {
+    int domain_id;
+
+    DDS_DomainParticipant *participant;
+    DDS_Publisher *publisher;
+    DDS_Topic *topic;
+    DDS_DataWriter *writer;
+    FBDataWriter *fb_writer;
+    const char *type_name;
+    FB *instance;
+    DDS_InstanceHandle_t instance_handle;
+};
+
 
 #define PLUGIN_NAME         "out_dds_unstr"
 
